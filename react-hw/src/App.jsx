c0839863d95af51a41e7131eb2c6c8114f6f661a
Dmytro Storoshchuk <dmutrostoroshchuk@gmail.com>
2026-05-21 21:33:53 +0300

    Add files via upload

--- react-hw/src/App.jsx
+++ react-hw/src/App.jsx
@@ -1,36 +1,64 @@
-/*
-Отримання та відображення завдання з API:
-Редагувати App компонент, 
-який виконує запит до API https://jsonplaceholder.typicode.com/todos/1
-і відображає дані завдання.
-
-Вимоги:
-- Використати useEffect для виконання запиту при першому рендері компонента.
-- Створити три стани за допомогою useState:
-    - task — для збереження отриманого завдання;
-    - error — для обробки помилок при запиті;
-    - loading — для індикації завантаження даних.
-- Відобразити:
-    - повідомлення про завантаження, коли loading === true;
-    - повідомлення про помилку, якщо error не порожній;
-    - інформацію про завдання, коли task успішно отриманий.
-- Використати fetch для запиту до API.
-*/
-
 import React from 'react'
 import './App.css'
 
 function App() {
 
+  
+  
   const [task, setTask] = React.useState(null);
+  
+
+  const [error, setError] = React.useState('');
+  
+
+  const [loading, setLoading] = React.useState(true);
 
   React.useEffect(() => {
 
-  }, []);
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then((response) => {
+
+        if (!response.ok) {
+          throw new Error('Не вдалося отримати дані');
+        }
+
+        return response.json();
+      })
+      .then((data) => {
+   
+        setTask(data); 
+        setLoading(false); 
+      })
+      .catch((err) => {
+        
+        setError(err.message); 
+        setLoading(false); 
+      });
+      
+  }, []); 
+
   
-  return (<div className='container'>
-    <h1>React Homework</h1>
-  </div>)
+  return (
+    <div className='container'>
+      <h1>React Homework</h1>
+
+      
+      {loading && <p>Завантаження даних...</p>}
+
+      
+      {error && <p style={{ color: 'red' }}>Помилка: {error}</p>}
+
+      
+      {task && !loading && (
+        <div>
+          <h3>Інформація про завдання:</h3>
+          <p><strong>Номер (ID):</strong> {task.id}</p>
+          <p><strong>Назва:</strong> {task.title}</p>
+          <p><strong>Статус:</strong> {task.completed ? '✅ Виконано' : '❌ Не виконано'}</p>
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default App;
