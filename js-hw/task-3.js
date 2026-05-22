7850d3ae3dac0e408199dc1e978f65e07e71da0f
Dmytro Storoshchuk <dmutrostoroshchuk@gmail.com>
2026-05-22 17:03:00 +0300

    Update task-3.js

--- js-hw/task-3.js
+++ js-hw/task-3.js
@@ -1,7 +1,9 @@
-/*
-    Сума чисел:
-    Дано масив чисел [1, 2, 2, 3, 4, 4]. 
-    Знайти суму.
-*/
-
 const numbers = [1, 2, 2, 3, 4, 4];
+
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+}
+
+console.log(sum);
