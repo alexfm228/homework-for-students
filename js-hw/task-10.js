0e5a1c0e893e03595984ec06b0a74489033887c1
Dmytro Storoshchuk <dmutrostoroshchuk@gmail.com>
2026-05-22 17:05:51 +0300

    Update task-10.js

--- js-hw/task-10.js
+++ js-hw/task-10.js
@@ -1,14 +1,11 @@
-/*
-    3. Простий Promise:
-    Є число x. 
-    Якщо воно більше 10 — Promise має повернути resolve("Більше 10").
-    Якщо менше або дорівнює 10 — reject("Менше або дорівнює 10").
-*/
-
 const x = 12;
 
 const checkNumber = new Promise((resolve, reject) => {
-    // ...
+    if (x > 10) {
+        resolve("Більше 10");
+    } else {
+        reject("Менше або дорівнює 10");
+    }
 });
 
 checkNumber
@@ -17,4 +14,4 @@ checkNumber
     })
     .catch((error) => {
         console.log("Помилка:", error);
-    }); 
+    });
