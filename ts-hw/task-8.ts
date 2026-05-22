628da038372ece019f8c512e2066d70f00025e0c
Dmytro Storoshchuk <dmutrostoroshchuk@gmail.com>
2026-05-22 16:58:30 +0300

    Update task-8.ts

--- ts-hw/task-8.ts
+++ ts-hw/task-8.ts
@@ -1,12 +1,10 @@
-/*
-    Функція для створення об'єкта Book:
-    Створити інтерфейс Book з ключами:
-    title (string), pages (number)
-    
-    Написати функцію createBook, яка приймає title і pages
-    і повертає об'єкт типу Book.
+interface Book {
+  title: string;
+  pages: number;
+}
 
-    Приклад використання:
-    const myBook: Book = createBook("TypeScript для початківців", 120);
-*/
+function createBook(title: string, pages: number): Book {
+  return { title, pages };
+}
 
+const myBook: Book = createBook("TypeScript для початківців", 120);
