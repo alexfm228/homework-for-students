078daaf9f82267c40929cda2c4977ee06edbcf48
Dmytro Storoshchuk <dmutrostoroshchuk@gmail.com>
2026-05-22 16:57:17 +0300

    Update task-5.ts

--- ts-hw/task-5.ts
+++ ts-hw/task-5.ts
@@ -1,15 +1,17 @@
-/*
-    Типізація об’єкта з вкладеним об’єктом:
-    Створи інтерфейс Person з ключами:
-    name (string), age (number), address (об'єкт з city і street)
-    та призначити його тип об'єкту person.
-*/
+interface Person {
+  name: string;
+  age: number;
+  address: {
+    city: string;
+    street: string;
+  };
+}
 
-const person = {
-    name: "Alex",
-    age: 25,
-    address: {
-        city: "Київ",
-        street: "Хрещатик"
-    }
+const person: Person = {
+  name: "Alex",
+  age: 25,
+  address: {
+    city: "Київ",
+    street: "Хрещатик"
+  }
 };
