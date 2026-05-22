a806f5e3209f0af915de0956112b767402c247ca
Dmytro Storoshchuk <dmutrostoroshchuk@gmail.com>
2026-05-22 17:02:27 +0300

    Update task-1.js

--- js-hw/task-1.js
+++ js-hw/task-1.js
@@ -1,9 +1,14 @@
-/*
-    Створення об'єкта:
-    Створити об'єкт з ключами name, age, 
-    а також вкладений об'єкт address з ключами city і street.
-*/
-
 const person = {
-    
+    name: "Дмитро",
+    age: 19,
+    address: {
+        city: "Львів",
+        street: "Хмельницького"
+    }
 };
+
+console.log(person);
+console.log("Ім'я:", person.name);
+console.log("Вік:", person.age);
+console.log("Місто:", person.address.city);
+console.log("Вулиця:", person.address.street);
