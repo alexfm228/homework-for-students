5a5f95fc77e79d64a3177ed79a561fd2d4d95d96
Dmytro Storoshchuk <dmutrostoroshchuk@gmail.com>
2026-05-22 17:03:31 +0300

    Update task-4.js

--- js-hw/task-4.js
+++ js-hw/task-4.js
@@ -1,8 +1,9 @@
-/*
-    Множення чисел на 2:
-    Дано масив чисел [1, 2, 3, 4, 5]. 
-    Помножити всі елементи на 2.
-    Приклад [1, 2, 3, 4, 5] → [2, 4, 6, 8, 10].
-*/
-
 const numbers = [1, 2, 3, 4, 5];
+
+const result = [];
+
+for (let i = 0; i < numbers.length; i++) {
+    result.push(numbers[i] * 2);
+}
+
+console.log(result);
